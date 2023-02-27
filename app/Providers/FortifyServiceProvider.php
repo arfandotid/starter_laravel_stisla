fb3d94aa8dd16d8e4298fa38cab6ac72b733507e
arfandotid <arfandotid@gmail.com>
2023-02-27 22:32:53 +0700

    register

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -45,5 +45,9 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::loginView(function() {
             return view('pages.auth.login');
         });
+
+        Fortify::registerView(function() {
+            return view('pages.auth.register');
+        });
     }
 }
--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -11,90 +11,51 @@
                         <img src="../assets/img/stisla-fill.svg" alt="logo" width="100"
                             class="shadow-light rounded-circle">
                     </div>
-
                     <div class="card card-primary">
                         <div class="card-header">
                             <h4>Register</h4>
                         </div>
-
                         <div class="card-body">
-                            <form method="POST">
-                                <div class="row">
-                                    <div class="form-group col-6">
-                                        <label for="first_name">First Name</label>
-                                        <input id="first_name" type="text" class="form-control" name="first_name"
-                                            autofocus>
-                                    </div>
-                                    <div class="form-group col-6">
-                                        <label for="last_name">Last Name</label>
-                                        <input id="last_name" type="text" class="form-control" name="last_name">
-                                    </div>
+                            <form method="POST" action="{{ route('register') }}" autocomplete="off">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input value="{{ old('name') }}" id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" autofocus>
+                                    @error('name')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-
                                 <div class="form-group">
                                     <label for="email">Email</label>
-                                    <input id="email" type="email" class="form-control" name="email">
-                                    <div class="invalid-feedback">
-                                    </div>
+                                    <input value="{{ old('email') }}" id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email">
+                                    @error('email')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
                                 </div>
-
                                 <div class="row">
                                     <div class="form-group col-6">
                                         <label for="password" class="d-block">Password</label>
-                                        <input id="password" type="password" class="form-control pwstrength"
-                                            data-indicator="pwindicator" name="password">
-                                        <div id="pwindicator" class="pwindicator">
-                                            <div class="bar"></div>
-                                            <div class="label"></div>
-                                        </div>
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+                                        @error('password')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
                                     </div>
                                     <div class="form-group col-6">
-                                        <label for="password2" class="d-block">Password Confirmation</label>
-                                        <input id="password2" type="password" class="form-control" name="password-confirm">
+                                        <label for="password_confirmation" class="d-block">Password Confirmation</label>
+                                        <input id="password_confirmation" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation">
+                                        @error('password_confirmation')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
                                     </div>
                                 </div>
-
-                                <div class="form-divider">
-                                    Your Home
-                                </div>
-                                <div class="row">
-                                    <div class="form-group col-6">
-                                        <label>Country</label>
-                                        <select class="form-control selectric">
-                                            <option>Indonesia</option>
-                                            <option>Palestine</option>
-                                            <option>Syria</option>
-                                            <option>Malaysia</option>
-                                            <option>Thailand</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-6">
-                                        <label>Province</label>
-                                        <select class="form-control selectric">
-                                            <option>West Java</option>
-                                            <option>East Java</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="form-group col-6">
-                                        <label>City</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                    <div class="form-group col-6">
-                                        <label>Postal Code</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" name="agree" class="custom-control-input" id="agree">
-                                        <label class="custom-control-label" for="agree">I agree with the terms and
-                                            conditions</label>
-                                    </div>
-                                </div>
-
                                 <div class="form-group">
                                     <button type="submit" class="btn btn-primary btn-lg btn-block">
                                         Register
@@ -104,7 +65,7 @@
                         </div>
                     </div>
                     <div class="simple-footer">
-                        Copyright &copy; Stisla 2018
+                        Copyright {{ date('Y') }} &copy; {{ env('APP_NAME') }} &bull; Theme by Stisla
                     </div>
                 </div>
             </div>
